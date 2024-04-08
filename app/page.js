3f2616bcf8cc3cb33df89127a7ce1e77e9b3d17e
Sharif Bey <sbey01@syr.edu>
2024-04-08 13:22:30 -0400

    environment var

--- app/page.js
+++ app/page.js
@@ -1,46 +1,75 @@
 "use client";
 
 import { useState, useEffect } from "react";
+
 import Image from "next/image";
+
 import ButtonDemo from "../components/ButtonDemo";
 import ColorPicker from "../components/ColorPicker";
 import PeoplePicker from "../components/PeoplePicker";
 
-import { getPeople, getWeatherData } from "../lib/api";
+import {
+  getGeoLocation,
+  getPeople,
+  getWeatherData,
+  getWeatherDataByLatLon,
+} from "../lib/api";
 
 const Homepage = () => {
   const [weatherData, setWeatherData] = useState(null);
+  const [location, setLocation] = useState(null);
+  const [errorMsg, setErrorMsg] = useState(null);
+
   const peopleArr = getPeople();
 
   useEffect(() => {
+    getGeoLocation()
+      .then((position) => {
+        console.log(position);
+        setLocation(position);
+      })
+      .catch((error) => {
+        setErrorMsg(error);
+      });
+  }, []);
+
+  useEffect(() => {
     const fetchData = async () => {
-      const response = await getWeatherData();
+      const response = await getWeatherDataByLatLon(location);
       setWeatherData(response);
     };
-    fetchData();
-  }, []);
+    location ? fetchData() : null;
+  }, [location]);
+
+  // useEffect(() => {
+  //   const fetchData = async () => {
+  //     const response = await getWeatherData();
+  //     setWeatherData(response);
+  //   };
+  //   fetchData();
+  // }, []);
 
   //console.log({ peopleArr });
   return (
     <div>
       <h1>Weather app</h1>
+      {errorMsg && <div>{errorMsg}</div>}
       {weatherData && (
         <div>
           <h2>{weatherData.city.name}</h2>
           <p>Current temp: {weatherData.list[0].main.temp}&deg; F</p>
           <p>{weatherData.list[0].weather[0].description}</p>
           <Image
-            src={` https://openweathermap.org/img/wn/${weatherData.list[0].weather[0].icon}@2x.png`}
-            alt={`Weather Icon for ${weatherData.list[0].weather[0].description}`}
+            src={`https://openweathermap.org/img/wn/${weatherData.list[0].weather[0].icon}@2x.png`}
+            alt={`Weather icon for ${weatherData.list[0].weather[0].description}`}
             width={100}
             height={100}
           />
         </div>
       )}
-
-      {/* <PeoplePicker people={peopleArr} />
+      {/*<PeoplePicker people={peopleArr} />
       <ButtonDemo />
-      <ColorPicker /> */}
+  <ColorPicker />*/}
     </div>
   );
 };
